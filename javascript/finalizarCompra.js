0b493027172f74267d2125a893252bc6d16cd2ff
Gabriel <wormsgabriel@hotmail.com>
2023-07-27 23:01:27 -0300

    se agrego, una carpeta json con compras.json en la cual posteriormente se modifico compras.js, y se agrego promesas y asincronia

--- javascript/finalizarCompra.js
+++ javascript/finalizarCompra.js
@@ -3,62 +3,68 @@ const unidadElements = document.getElementById("unidades");
 const precioElements = document.getElementById("precio-final");
 const reiniciarElements = document.getElementById("reiniciar");
 
-function crearTarjetasProductosInicio() {
-  seccionDeTarjetas.innerHTML = "";
-  const productos = JSON.parse(localStorage.getItem("consolas"));
+const getProductos = async () => {
+  const response = await fetch("../json/compras.json");
+  const data = await response.json();
+  console.log(data);
+  function crearTarjetasProductosInicio() {
+    seccionDeTarjetas.innerHTML = "";
+    const productos = JSON.parse(localStorage.getItem("consolas"));
+    productos && productos.length > 0 ?
+      productos.forEach(producto => {
+        const nuevasConsolas = document.createElement("div");
+        nuevasConsolas.classList = "producto-finalizado";
+        nuevasConsolas.innerHTML = `
+          <img src=".${producto.img}">
+          <h3>${producto.consola}</h3>
+          <span class="producto-precio">$${producto.precio}</span>
+          <div>
+            <button>+</button>
+            <span class="cantidad">${producto.cantidad}</span>
+            <button>-</button>
+          </div>
+        `;
+        seccionDeTarjetas.appendChild(nuevasConsolas);
+        nuevasConsolas.getElementsByTagName("button")[0].addEventListener("click", (e)=> {
+          const contarElement = e.target.parentElement.getElementsByTagName("span")[0];
+          contarElement.innerText = agregarAlCarrito(producto);
+          actualizarTotal();
+        });
+        nuevasConsolas.getElementsByTagName("button")[1].addEventListener("click", (e)=> {
+          restarAlCarrito(producto);
+          crearTarjetasProductosInicio();
+          actualizarTotal();
+        });
+      }) : null;
+  }
   
-  productos && productos.length > 0 ?
-    productos.forEach(producto => {
-      const nuevasConsolas = document.createElement("div");
-      nuevasConsolas.classList = "producto-finalizado";
-      nuevasConsolas.innerHTML = `
-        <img src=".${producto.img}">
-        <h3>${producto.consola}</h3>
-        <span class="producto-precio">$${producto.precio}</span>
-        <div>
-          <button>+</button>
-          <span class="cantidad">${producto.cantidad}</span>
-          <button>-</button>
-        </div>
-      `;
-      seccionDeTarjetas.appendChild(nuevasConsolas);
-      nuevasConsolas.getElementsByTagName("button")[0].addEventListener("click", (e)=> {
-        const contarElement = e.target.parentElement.getElementsByTagName("span")[0];
-        contarElement.innerText = agregarAlCarrito(producto);
-        actualizarTotal();
-      });
-      nuevasConsolas.getElementsByTagName("button")[1].addEventListener("click", (e)=> {
-        restarAlCarrito(producto);
-        crearTarjetasProductosInicio();
-        actualizarTotal();
-      });
-    }) : null;
-}
-
-crearTarjetasProductosInicio(consolas);
-actualizarTotal();
-
-function actualizarTotal() {
-  const productos = JSON.parse(localStorage.getItem("consolas"));
-  let unidades = 0;
-  let precio = 0;
-  
-  productos && productos.length > 0 ?
-    productos.forEach(producto => {
-      unidades += producto.cantidad;
-      precio += producto.precio * producto.cantidad;
-    }) : null;
-  
-  unidadElements.innerText = unidades;
-  precioElements.innerText = precio;
-}
-
-reiniciarElements.addEventListener("click", reiniciarCarrito);
-function reiniciarCarrito() {
-  localStorage.removeItem("consolas");
+  crearTarjetasProductosInicio(data);
   actualizarTotal();
-  crearTarjetasProductosInicio();
+  
+  function actualizarTotal() {
+    const productos = JSON.parse(localStorage.getItem("consolas"));
+    let unidades = 0;
+    let precio = 0;
+    
+    productos && productos.length > 0 ?
+      productos.forEach(producto => {
+        unidades += producto.cantidad;
+        precio += producto.precio * producto.cantidad;
+      }) : null;
+    
+    unidadElements.innerText = unidades;
+    precioElements.innerText = precio;
+  }
+  
+  reiniciarElements.addEventListener("click", reiniciarCarrito);
+  function reiniciarCarrito() {
+    localStorage.removeItem("consolas");
+    actualizarTotal();
+    crearTarjetasProductosInicio();
+  }
+  
 }
+getProductos()
 
 const botonComprar = document.getElementsByClassName("boton-finalizacion")[0];
 
--- javascript/index.js
+++ javascript/index.js
@@ -1,12 +1,15 @@
 const seccionDeTarjetas = document.getElementById("productos-container");
 const mensajeVacioElements = document.getElementById("carrito-vacio");
 const totalesElements = document.getElementById("totales");
+const unidadElements = document.getElementById("unidades");
+const precioElements = document.getElementById("precio-final");
+const reiniciarElements = document.getElementById("reiniciar");
 
-
-
-function crearTarjetasProductosInicio(consolas) {
-  const productos = consolas;
-  productos.forEach((producto) => {
+const getConsolas = async() => {
+  const response = await fetch("../json/compras.json");
+  const data = await response.json();
+  console.log(data);
+  data.forEach((producto) => {
     const nuevasConsolas = document.createElement("div");
     nuevasConsolas.classList = "tarjeta-producto";
     nuevasConsolas.innerHTML = `
@@ -19,32 +22,33 @@ function crearTarjetasProductosInicio(consolas) {
     seccionDeTarjetas.appendChild(nuevasConsolas);
     nuevasConsolas.getElementsByClassName("sumarProducto")[0].addEventListener("click", () => agregarAlCarrito(producto));
   });
-}
+  Array.from(sumarProducto).forEach((boton) => {
+    boton.addEventListener("click", () => {
+      const consolas = JSON.parse(localStorage.getItem("consolas"));
+      Toastify({
+        text: "Producto Agregado",
+        duration: 1500,
+        destination: "https://github.com/apvarun/toastify-js",
+        newWindow: true,
+        close: false,
+        gravity: "top", // `top` or `bottom`
+        position: "right", // `left`, `center` or `right`
+        stopOnFocus: true, // Prevents dismissing of toast on hover
+        style: {
+          background: "#38b000",
+          fontFamily: "'Roboto', sans-serif"
+        },
+        onClick: function(){} // Callback after click
+      }).showToast();
+    })
+  })
+};
 
-crearTarjetasProductosInicio(consolas);
+getConsolas();
+
 
 const sumarProducto = document.getElementsByClassName("sumarProducto");
 
-Array.from(sumarProducto).forEach((boton) => {
-  boton.addEventListener("click", () => {
-    const productos = JSON.parse(localStorage.getItem("consolas"));
-    Toastify({
-      text: "Producto Agregado",
-      duration: 1500,
-      destination: "https://github.com/apvarun/toastify-js",
-      newWindow: true,
-      close: false,
-      gravity: "top", // `top` or `bottom`
-      position: "right", // `left`, `center` or `right`
-      stopOnFocus: true, // Prevents dismissing of toast on hover
-      style: {
-        background: "#38b000",
-        fontFamily: "'Roboto', sans-serif"
-      },
-      onClick: function(){} // Callback after click
-    }).showToast();
-  })
-})
 
 
 
